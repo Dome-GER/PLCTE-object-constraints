abedc84b171cc6173e39c892b4b1f97d5af2fd06
Dominic Sauer <dominic.sauer@yahoo.de>
2017-06-06 17:27:31 +0200

    Further (cleaner) implementation using the class method_missing pattern
     - Use class methd to define constraint
     - Call constraint with constraint-specific method; implementation using method_missing class method
     - Create new constraint objects each time the constraint method is called to enable reusability

--- delayed_constraint.rb
+++ delayed_constraint.rb
@@ -1,6 +1,21 @@
 require 'libz3'
 
 class DelayedConstraint
+	@@constraints = {}
+
+	def self.constraint(identifier, constraint_definition)
+		@@constraints[identifier] = constraint_definition
+	end
+
+	def self.method_missing(name, param_binding, context)
+    	unless @@constraints.key?(name.to_sym)
+    		raise ArgumentError, "Constraint does not exist"
+    	end
+
+
+    	self.new(@@constraints[name]).bind(param_binding, context)
+  	end
+
 	attr_reader :constraint
 	def initialize(constraint_definition)
 		@constraint = constraint_definition
